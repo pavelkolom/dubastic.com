42d67d984d902ee09a4c50a4447a38edce4e6bec
Sergey Shamanayev <shamanayev.sergey@gmail.com>
2020-08-26 20:51:57 +0400

    Update pages in footer, remove in headers

---
+++ AdsMaster.Mvc/Areas/Ads/Controllers/PageController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdsMaster.Mvc.Areas.Ads.Controllers
+{
+    [Area("Ads")]
+    public class PageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Faq()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Terms()
+        {
+            return View();
+        }
+    }
+}
