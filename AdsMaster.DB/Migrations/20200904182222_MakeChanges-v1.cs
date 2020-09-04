03fa5bde46f6ece4da8df6a0f02c396edc46ae55
Sergey Shamanayev <shamanayev.sergey@gmail.com>
2020-09-04 22:59:14 +0400

    add default value

---
+++ AdsMaster.DB/Migrations/20200904182222_MakeChanges-v1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AdsMaster.DB.Migrations
+{
+    public partial class MakeChangesv1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "IsModerated",
+                table: "pf_Topic",
+                nullable: false,
+                defaultValue: 0
+                );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
