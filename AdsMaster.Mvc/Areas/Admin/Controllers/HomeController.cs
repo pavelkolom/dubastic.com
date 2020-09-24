9e7a1dde6b1883c72af79cc7bce3f6a2118296d3
Sergey Shamanayev <shamanayev.sergey@gmail.com>
2020-09-24 20:25:35 +0400

    Add area admin and implement moderate

---
+++ AdsMaster.Mvc/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PopForums.Mvc.Areas.Forums.Authorization;
+
+namespace AdsMaster.Mvc.Areas.Admin.Controllers
+{
+    [Authorize(Policy = PopForums.Models.PermanentRoles.Admin, AuthenticationSchemes = PopForumsAuthorizationDefaults.AuthenticationScheme)]
+    [Area("Admin")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
